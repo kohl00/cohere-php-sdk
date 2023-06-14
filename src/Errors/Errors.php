11a37745a998cc23bd15ad44fa5a20b61ef27020
Kohl Kohlbrenner <kohlkohlbrenner@Kohls-MBP.attlocal.net>
2023-06-13 19:59:24 -0500

    Refactoring Errors.php. Adding typehints and strict type declaration.

--- src/Errors/Errors.php
+++ src/Errors/Errors.php
@@ -1,34 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Cohere\Errors;
 
 use Exception;
 
 class CohereError extends Exception {
-    private $message;
-
-    public function __construct($message = null) {
-        parent::__construct($message);
-        $this->message = $message;
-    }
-
-    public function __toString() {
-        $msg = $this->message ?? "<empty message>";
-        return $msg;
+    public function __toString(): string {
+        return $this->message;
     }
 }
 
 class CohereAPIError extends CohereError {
-    private $http_status;
-    private $headers;
+    private ?int $http_status = null;
+    private array $headers = [];
 
-    public function __construct($message = null, $http_status = null, $headers = null) {
+    public function __construct(?string $message = null, ?int $http_status = null, ?array $headers = null) {
         parent::__construct($message);
         $this->http_status = $http_status;
         $this->headers = $headers ?? [];
     }
 
-    public static function fromResponse($response, $message = null) {
+    public static function fromResponse($response, ?string $message = null): self {
         return new self($message ?? $response->getBody()->getContents(), $response->getStatusCode(), $response->getHeaders());
     }
 }
